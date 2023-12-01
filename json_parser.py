1df7188af95b29fa84bdd4437f5064c19064147b
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2023-11-30 22:14:09 -0300

    adding multiple column support

--- json_parser.py
+++ json_parser.py
@@ -6,7 +6,6 @@ def load_data():
     
     current_directory = os.getcwd()
 
-
     for filename in os.listdir(current_directory):
         if filename.endswith(".json"):
             table_name = os.path.splitext(filename)[0]
@@ -18,37 +17,32 @@ def load_data():
 
     return data
 
-def parse_query(query,data):
+def parse_query(query, data):
 
     if query.startswith("PEGAR") and "DE" in query:
         select_clause, from_clause = query.split("DE", 1)
-
-        column_name = select_clause.split("PEGAR")[1].strip()
+        columns = [col.strip() for col in select_clause.split("PEGAR")[1].split(",")]
 
         table = from_clause.strip()
 
         print(f"Query: {query}")
-        print(f"Column Name: {column_name}")
+        print(f"Columns: {columns}")
         print(f"Table: {table}")
 
-        result = execute_query(column_name,table,data)
+        result = execute_query(columns, table, data)
         return result
     else:
         return "Sintaxe inválida"
     
-def execute_query(column_name,table,data):
-
+def execute_query(columns, table, data):
+    
     with open(f"{table}.json",'r') as jsonfile:
         table_data = json.load(jsonfile).get(table, [])
-
-        result = [row.get(column_name) for row in table_data]
+        result = [{col: row.get(col) for col in columns} for row in table_data]
 
         return result
-    
 
-json_query = "PEGAR Average DE hurricanes"
+json_query = "PEGAR Month, Average DE hurricanes"
 data = load_data()
 result=parse_query(json_query,data)
 print(result)
-    
-
